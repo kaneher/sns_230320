ad0f96c93562e2699f27f32cffa1d20f4b7449ee
test <test@test.com>
2023-07-18 18:49:18 +0900

    수업

--- src/main/java/com/sns/user/UserController.java
+++ src/main/java/com/sns/user/UserController.java
@@ -1,5 +1,7 @@
 package com.sns.user;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,5 +22,30 @@ public class UserController {
 		return "template/layout";
 	}
 	
+	/**
+	 * 로그인 화면
+	 * @param model
+	 * @return
+	 */
+	@GetMapping("/sign_in_view")
+	public String signInView(Model model) {
+		model.addAttribute("view", "user/signIn");
+		return "template/layout";
+	}
 	
+	/**
+	 * 로그아웃
+	 * @param session
+	 * @return
+	 */
+	@RequestMapping("/sign_out")
+	public String signOut(HttpSession session) {
+		// 세션에 있는 내용을 비운다.
+		session.removeAttribute("userId");
+		session.removeAttribute("userLoginId");
+		session.removeAttribute("userName");
+		
+		// 로그인 화면으로 이동 => redirect
+		return "redirect:/user/sign_in_view";
+	}
 }
