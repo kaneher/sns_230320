dd68e3aa8a6049351241140c8221a4abc0d3bc6e
test <test@test.com>
2023-07-20 18:43:53 +0900

    수업

---
+++ src/main/java/com/sns/post/PostRestController.java
@@ -0,0 +1,31 @@
+package com.sns.post;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.sns.post.bo.PostBO;
+
+@RestController
+@RequestMapping("/post")
+public class PostRestController {
+
+	@Autowired
+	private PostBO postBO;
+	
+	@PostMapping("/create")
+	public Map<String, Object> create(
+			@RequestParam("content") String content,
+			@RequestParam(value = "file") MultipartFile file,
+			HttpSession session) {
+		
+		
+	}
+}
