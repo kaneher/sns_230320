c1b30fe6f651d766b75cf7058dc586d9bb60e0b4
test <test@test.com>
2023-07-26 18:42:51 +0900

    수업

--- src/main/java/com/sns/like/bo/LikeBO.java
+++ src/main/java/com/sns/like/bo/LikeBO.java
@@ -15,11 +15,29 @@ public class LikeBO {
 	// output : void
 	public void likeToggle(int postId, int userId) {
 		// 셀렉트 => count(*)
-		int like = likeMapper.selectLikeCountByPostIdAndUserId(postId, userId);
+		int like = likeMapper.selectLikeCountByPostIdOrUserId(postId, userId);
 		if (like > 0) { // 존재 -> delete
 			likeMapper.deleteLikeByPostIdUserId(postId, userId);
 		} else { // 없음 -> insert
 			likeMapper.insertLike(postId, userId);
 		}
+	}
+	
+	// input : postId
+	// output : 좋아요 개수(int)
+	public int getLikeCountByPostId(int postId) {
+		return likeMapper.selectLikeCountByPostIdOrUserId(postId, null);
+	}
+	
+	// input : postId, userId
+	// output : 좋아요 여부 boolean
+	public boolean filledLike(int postId, Integer userId) {
+		// 비로그인
+		if (userId == null) {
+			return false;
+		}
+		
+		// 로그인
+		return likeMapper.selectLikeCountByPostIdOrUserId(postId, userId) > 0;
 	}
 }
--- src/main/java/com/sns/like/dao/LikeMapper.java
+++ src/main/java/com/sns/like/dao/LikeMapper.java
@@ -6,9 +6,17 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface LikeMapper {
 
-	public int selectLikeCountByPostIdAndUserId(
+//	public int selectLikeCountByPostIdAndUserId(
+//			@Param("postId") int postId,
+//			@Param("userId") int userId);
+	
+	
+//	public int selectLikeCountByPostId(int postId);
+	
+	// by postId userId, by postId => 하나의 쿼리로 합친다.
+	public int selectLikeCountByPostIdOrUserId(
 			@Param("postId") int postId, 
-			@Param("userId") int userId);
+			@Param("userId") Integer userId);
 	
 	public void insertLike(
 			@Param("postId") int postId, 
